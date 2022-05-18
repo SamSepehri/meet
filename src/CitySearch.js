1a44231fbe7bbbc178aa09978cc66424d0613927
samisep <sam.sepehri@gmx.de>
2022-05-19 00:50:17 +0200

    update

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -17,6 +17,12 @@ class CitySearch extends Component {
         });
     };
 
+    handleItemClicked = (suggestion) => {
+        this.setState({
+            query: suggestion
+        });
+    }
+
     render() {
         return (
             <div className="CitySearch">
@@ -28,9 +34,12 @@ class CitySearch extends Component {
                 />
                 <ul className="suggestions">
                     {this.state.suggestions.map((suggestion) => (
-                        <li key={suggestion}>{suggestion}</li>
+                        <li
+                            key={suggestion}
+                            onClick={() => this.handleItemClicked(suggestion)}
+                        >{suggestion}</li>
                     ))}
-                    <li key='all'>
+                    <li>
                         <b>See all cities</b>
                     </li>
                 </ul>
