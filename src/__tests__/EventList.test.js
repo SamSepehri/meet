40d94ae8b405b24d090b0162265a2514219daf09
samisep <sam.sepehri@gmx.de>
2022-05-19 00:53:25 +0200

    update

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -4,12 +4,14 @@ import EventList from '../EventList';
 import Event from '../Event';
 import App from '../App';
 import CitySearch from '../CitySearch'
+import { mockData } from '../mock-data';
+
 
 describe('<EventList /> component', () => {
 
     test('render correct number of events', () => {
-        const EventListWrapper = shallow(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
-        expect(EventListWrapper.find(Event)).toHaveLength(4);
+        const EventListWrapper = shallow(<EventList events={mockData} />);
+        expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
     });
 
     test('render CitySearch', () => {
