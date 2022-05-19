4dfe5c7871ac6ce8418dbff526f1a74ccda13b8e
samisep <sam.sepehri@gmx.de>
2022-05-19 16:12:38 +0200

    update

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,17 +1,41 @@
-import React, { Component } from 'react';
+//src/NumberOfEvents.js
+
+import React, { Component } from "react";
+import { ErrorAlert } from "./Alert";
 
 class NumberOfEvents extends Component {
+    constructor() {
+        super();
+        // stting the values for the default states
+        this.state = {
+            numberOfEvents: 32,
+            infoText: "",
+        };
+    }
+
+    updateNumberOfEvents = (event) => {
+        this.setState({
+            numberOfEvents: event.target.value,
+            infoText: "",
+        });
+        if (event.target.value > 0 && event.target.value < 33) {
+            this.props.updateEventNumbers(event.target.value);
+        } else {
+            this.setState({ infoText: "Select a number between 1 and 32!" });
+        }
+    };
 
     render() {
         return (
-            <div className="NumberOfEvents">
-                <h4>Number of events to display</h4>
+            <div className="numberOfEvents">
+                <ErrorAlert text={this.state.infoText} />
+                <label className="numberOfEvents__lable">Number of Events</label>{" "}
                 <input
-                    className="number-of-events"
                     type="number"
-                    value={this.props.numberOfEvents}
-                    onChange={(e) => this.props.updateNumberOfEvents(e)}>
-                </input>
+                    className="numberOfEvents__input"
+                    value={this.state.numberOfEvents}
+                    onChange={this.updateNumberOfEvents}
+                ></input>
             </div>
         );
     }
