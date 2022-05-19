351f8d1eaa7a8260e4eaddf8a93b66316886efb3
samisep <sam.sepehri@gmx.de>
2022-05-19 10:05:34 +0200

    update

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -20,8 +20,10 @@ class CitySearch extends Component {
 
     handleItemClicked = (suggestion) => {
         this.setState({
-            query: suggestion
+            query: suggestion,
+            showSuggestions: false
         });
+
         this.props.updateEvents(suggestion);
     }
 
@@ -35,7 +37,7 @@ class CitySearch extends Component {
                     onChange={this.handleInputChanged}
                     onFocus={() => { this.setState({ showSuggestions: true }) }}
                 />
-                <ul className="suggestions">
+                <ul className="suggestions" style={this.state.showSuggestions ? {} : { display: 'none' }}>
                     {this.state.suggestions.map((suggestion) => (
                         <li
                             key={suggestion}
