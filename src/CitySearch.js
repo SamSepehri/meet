d234b9aa59f6bd20b1e0360b1d10074506522e4b
samisep <sam.sepehri@gmx.de>
2022-05-19 15:22:51 +0200

    update

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
     state = {
         query: '',
         suggestions: [],
-        showSuggestions: undefined
+        showSuggestions: false,
+        infoText: ''
     }
 
     handleInputChanged = (event) => {
         const value = event.target.value;
+        this.setState({ showSuggestions: true });
         const suggestions = this.props.locations.filter((location) => {
             return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
         });
-        this.setState({
-            query: value,
-            suggestions,
-        });
+        if (suggestions.length === 0) {
+            this.setState({
+                query: value,
+                infoText: 'We can not find the city you are looking for. Please try another city',
+            });
+        } else {
+            return this.setState({
+                query: value,
+                suggestions,
+                infoText: ''
+            });
+        }
     };
 
     handleItemClicked = (suggestion) => {
@@ -30,12 +41,15 @@ class CitySearch extends Component {
     render() {
         return (
             <div className="CitySearch">
+                <InfoAlert text={this.state.infoText} />
+
                 <input
                     type="text"
                     className="city"
                     value={this.state.query}
                     onChange={this.handleInputChanged}
                     onFocus={() => { this.setState({ showSuggestions: true }) }}
+
                 />
                 <ul className="suggestions" style={this.state.showSuggestions ? {} : { display: 'none' }}>
                     {this.state.suggestions.map((suggestion) => (
